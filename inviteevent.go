50492852cc98558a24f468f9797c068aa1dc895e
Kegan Dougal <kegan@matrix.org>
2017-05-15 17:33:53 +0100

    No room ID

--- inviteevent.go
+++ inviteevent.go
@@ -4,7 +4,6 @@ package gomatrixserverlib
 // the 'invite_state' of a /sync response. It is not a real event.
 type InviteStateFormattedEvent struct {
 	Content  rawJSON `json:"content"`
-	RoomID   string  `json:"room_id,omitempty"`
 	Sender   string  `json:"sender"`
 	StateKey *string `json:"state_key,omitempty"`
 	Type     string  `json:"type"`
