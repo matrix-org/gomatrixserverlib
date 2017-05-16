106fda926c2da455b61882dd4027199c1c8b81fa
Mark Haines <mjark@negativecurvature.net>
2017-05-16 17:47:10 +0100

    Kill of EDUs until I've read enough of synapse to know what they are

--- transaction.go
+++ transaction.go
@@ -25,9 +25,6 @@ type Transaction struct {
 	// by this transaction. The events should either be events that originate
 	// on the origin server or be join m.room.member events.
 	PDUs []Event `json:"pdus"`
-	// The ephemeral events pushed from the origin server to the destinaton
-	// server by this transaction.
-	EDUs []EDU `json:"edus"`
 }
 
 // A TransactionID identifies a transaction sent by a matrix server to another
@@ -36,42 +33,3 @@ type Transaction struct {
 // The ID must be safe to insert into a URL path segment. The ID should have a
 // format matching '^[0-9A-Za-z\-_]*$'
 type TransactionID string
-
-// An EDU is used to transmit ephemeral data such as presence and typing from
-// one matrix server to another.
-type EDU struct {
-	// The type of the EDU, this tells the receiver how to interpret the
-	// contents.
-	EDUType string
-	// The JSON content of the EDU.
-	Content []byte
-}
-
-// SetContent sets the JSON "content" of an EDU.
-// Returns an error if there was a problem encoding the JSON.
-func (e *EDU) SetContent(content interface{}) (err error) {
-	e.Content, err = json.Marshal(content)
-	return
-}
-
-type eduFields struct {
-	EDUType string  `json:"edu_type"`
-	Content rawJSON `json:"content"`
-}
-
-// MarshalJSON implements json.Marshaller
-func (e EDU) MarshalJSON() (data []byte, err error) {
-	data, err = json.Marshal(eduFields{e.EDUType, rawJSON(e.Content)})
-	return
-}
-
-// UnmarshalJSON implements json.Unmarshaller
-func (e *EDU) UnmarshalJSON(data []byte) error {
-	var fields eduFields
-	if err := json.Unmarshal(data, &fields); err != nil {
-		return err
-	}
-	e.EDUType = fields.EDUType
-	e.Content = []byte(fields.Content)
-	return nil
-}
