da1d62e7a0dcacb4e3cd6dbb245c195b6e6ec2ba
Erik Johnston <erikj@matrix.org>
2017-09-22 14:58:19 +0100

    Typo

--- hex_string.go
+++ hex_string.go
@@ -25,7 +25,7 @@ import (
 // When the bytes are unmarshalled from JSON they are decoded from hex.
 type HexString []byte
 
-// MarshalJSON encodes the bytes as base64 and then encodes the hex as a JSON string.
+// MarshalJSON encodes the bytes as hex and then encodes the hex as a JSON string.
 // This takes a value receiver so that maps and slices of HexString encode correctly.
 func (h HexString) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(h))
