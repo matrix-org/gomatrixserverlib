5f8a5862dcedb6e3bc4aed965a3f1e3fbca8a712
Andrew Morgan <andrew@amorgan.xyz>
2019-02-06 19:19:58 +0000

    Fix ValidCertificates check not working when using well-known

--- certificates.go
+++ certificates.go
@@ -2,13 +2,18 @@ package gomatrixserverlib
 
 import (
 	"crypto/x509"
+	"strings"
 )
 
 // IsValidCertificate checks if the given x509 certificate can be verified using
 // system root CAs and an optional pool of intermediate CAs.
 func IsValidCertificate(serverName ServerName, c *x509.Certificate, intermediates *x509.CertPool) (valid bool, err error) {
+	// Remove port from serverName if it exists
+	serverNameCleaned := strings.Split(string(serverName), ":")[0]
+
+	// Check certificate chain validity
 	verificationOpts := x509.VerifyOptions{
-		DNSName:       string(serverName),
+		DNSName:       serverNameCleaned,
 		Intermediates: intermediates,
 	}
 	roots, err := c.Verify(verificationOpts)
