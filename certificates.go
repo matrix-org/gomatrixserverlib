ed9e8ab4903f9128a671375bec323f4f77180b97
Andrew Morgan <andrew@amorgan.xyz>
2019-02-07 12:31:08 +0000

    Error on checking certificate validity of an IP literal

--- certificates.go
+++ certificates.go
@@ -2,12 +2,20 @@ package gomatrixserverlib
 
 import (
 	"crypto/x509"
+	"errors"
+	"net"
 	"strings"
 )
 
 // IsValidCertificate checks if the given x509 certificate can be verified using
 // system root CAs and an optional pool of intermediate CAs.
 func IsValidCertificate(serverName ServerName, c *x509.Certificate, intermediates *x509.CertPool) (valid bool, err error) {
+	// Fail if serverName is an ipv4/6
+	if net.ParseIP(string(serverName)) != nil {
+		// IP Address
+		return false, errors.New("serverName is an IP literal")
+	}
+
 	// Remove port from serverName if it exists
 	serverNameCleaned := strings.Split(string(serverName), ":")[0]
 
