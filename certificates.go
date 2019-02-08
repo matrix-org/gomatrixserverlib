e5d7d49f0578e8ec3ecf6420cc190fd953f79f58
Andrew Morgan <andrew@amorgan.xyz>
2019-02-08 14:36:46 +0000

    Only support validating certs of domain names (not IPs). Add tests

--- certificates.go
+++ certificates.go
@@ -10,14 +10,11 @@ import (
 // IsValidCertificate checks if the given x509 certificate can be verified using
 // system root CAs and an optional pool of intermediate CAs.
 func IsValidCertificate(serverName ServerName, c *x509.Certificate, intermediates *x509.CertPool) (valid bool, err error) {
-	// Fail if serverName is an ipv4/6
-	if net.ParseIP(string(serverName)) != nil {
-		// IP Address
-		return false, errors.New("serverName is an IP literal")
+	// Clean and verify serverName
+	serverNameCleaned, err := cleanAndVerifyServerName(serverName)
+	if err != nil {
+		return false, err
 	}
-
-	// Remove port from serverName if it exists
-	serverNameCleaned := strings.Split(string(serverName), ":")[0]
 
 	// Check certificate chain validity
 	verificationOpts := x509.VerifyOptions{
@@ -27,4 +24,22 @@ func IsValidCertificate(serverName ServerName, c *x509.Certificate, intermediate
 	roots, err := c.Verify(verificationOpts)
 
 	return len(roots) > 0, err
+}
+
+// cleanServerName is a function that takes in a ServerName, verifies it, and
+// removes the port if necessary
+// TODO: Support IP Addresses (and update test)
+func cleanAndVerifyServerName(serverName ServerName) (serverNameCleaned string, err error) {
+	serverNameCleaned = string(serverName)
+
+	// Remove port from serverName if it exists
+	serverNameCleaned = strings.Split(string(serverName), ":")[0]
+	 
+	// Fail if serverName is an ipv4/6
+	if strings.Contains(serverNameCleaned, "[") || net.ParseIP(serverNameCleaned) != nil {
+		// This is an IP Address, fail
+		return "", errors.New("serverName is an IP literal. This is not currently supported for certificate validation checking.")
+	}
+
+	return serverNameCleaned, nil
 }
