27302f25e21828588ff7c24aaab082530c81e181
Neil Alexander <neilalexander@users.noreply.github.com>
2020-03-06 11:11:29 +0000

    Fix lint issue

--- stateresolutionv2.go
+++ stateresolutionv2.go
@@ -296,15 +296,13 @@ func (r *stateResolverV2) getFirstPowerLevelMainlineEvent(event Event) (
 // also apply them on top of the partial state. If they fail auth checks then
 // the event is ignored and dropped. Returns two lists - the first contains the
 // accepted (authed) events and the second contains the rejected events.
-func (r *stateResolverV2) authAndApplyEvents(events []Event) (accepted []Event) {
+func (r *stateResolverV2) authAndApplyEvents(events []Event) {
 	for _, event := range events {
 		// Check if the event is allowed based on the current partial state. If the
 		// event isn't allowed then simply ignore it and process the next one.
 		if err := Allowed(event, r); err != nil {
 			continue
 		}
-		// We've now authed the event.
-		accepted = append(accepted, event)
 		// Apply the newly authed event to the partial state. We need to do this
 		// here so that the next loop will have partial state to auth against.
 		r.applyEvents([]Event{event})
