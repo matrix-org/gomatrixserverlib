f283d7b6e509f4309a3abe107679537d334a57e7
Neil Alexander <neilalexander@users.noreply.github.com>
2020-03-10 08:57:53 +0000

    Room version as string due to room version grammar rules

--- eventversion.go
+++ eventversion.go
@@ -1,7 +1,7 @@
 package gomatrixserverlib
 
 // RoomVersion refers to the room version for a specific room.
-type RoomVersion int
+type RoomVersion string
 
 // StateResAlgorithm refers to a version of the state resolution algorithm.
 type StateResAlgorithm int
@@ -9,13 +9,15 @@ type StateResAlgorithm int
 // EventIDFormat refers to the formatting used to generate new event IDs.
 type EventIDFormat int
 
-// Room version constants
+// Room version constants. These are strings because the version grammar
+// allows for future expansion.
+// https://matrix.org/docs/spec/#room-version-grammar
 const (
-	RoomVersionV1 RoomVersion = iota + 1
-	RoomVersionV2
-	RoomVersionV3
-	RoomVersionV4
-	RoomVersionV5
+	RoomVersionV1 RoomVersion = "1"
+	RoomVersionV2 RoomVersion = "2"
+	RoomVersionV3 RoomVersion = "3"
+	RoomVersionV4 RoomVersion = "4"
+	RoomVersionV5 RoomVersion = "5"
 )
 
 // Event ID format constants
@@ -65,27 +67,27 @@ func (v RoomVersion) EnforceSignatureChecks() bool {
 }
 
 var roomVersionMeta = map[RoomVersion]roomVersion{
-	1: roomVersion{
+	"1": roomVersion{
 		stateResAlgorithm:      StateResV1,
 		eventIDFormat:          EventIDFormatV1,
 		enforceSignatureChecks: false,
 	},
-	2: roomVersion{
+	"2": roomVersion{
 		stateResAlgorithm:      StateResV2,
 		eventIDFormat:          EventIDFormatV1,
 		enforceSignatureChecks: false,
 	},
-	3: roomVersion{
+	"3": roomVersion{
 		stateResAlgorithm:      StateResV2,
 		eventIDFormat:          EventIDFormatV2,
 		enforceSignatureChecks: false,
 	},
-	4: roomVersion{
+	"4": roomVersion{
 		stateResAlgorithm:      StateResV2,
 		eventIDFormat:          EventIDFormatV3,
 		enforceSignatureChecks: false,
 	},
-	5: roomVersion{
+	"5": roomVersion{
 		stateResAlgorithm:      StateResV2,
 		eventIDFormat:          EventIDFormatV3,
 		enforceSignatureChecks: true,
