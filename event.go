ac84b78b81be41bb3ab898ec8f65e3893419a7ff
Neil Alexander <neilalexander@users.noreply.github.com>
2020-03-11 11:00:21 +0000

    Add PrepareAs

--- event.go
+++ event.go
@@ -346,6 +346,22 @@ func NewEventFromTrustedJSON(eventJSON []byte, redacted bool, roomVersion RoomVe
 	return
 }
 
+// PrepareAs populates the event fields struct for a specific room
+// version. This should only be used in cases where unmarshalling a parent
+// object results in an unmarshalled event.
+func (e *Event) PrepareAs(roomVersion RoomVersion) error {
+	if e.fields != nil {
+		return errors.New("gomatrixserverlib: can't prepare once populated")
+	}
+	switch roomVersion {
+	case RoomVersionV1, RoomVersionV2:
+		e.fields = eventFieldsRoomV1{}
+	case RoomVersionV3, RoomVersionV4, RoomVersionV5:
+		e.fields = eventFieldsRoomV3{}
+	}
+	return nil
+}
+
 // Redacted returns whether the event is redacted.
 func (e Event) Redacted() bool { return e.redacted }
 
