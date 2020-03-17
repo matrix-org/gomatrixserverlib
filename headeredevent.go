134415251e652abdd017584266ca62718f08cc00
Neil Alexander <neilalexander@users.noreply.github.com>
2020-03-17 10:32:36 +0000

    Room version metadata and HeaderedEvent (#158)
    * Define room version characteristics and types
    
    * Fix comment
    
    * Panic if room version is unsupported
    
    * Error handling for versions
    
    * Add HeaderedEvent
    
    * Add Event.Headered
    
    * Fix headered event unmarshalling
    
    * Better marshalling of HeaderedEvent
    
    * Remove header events before unmarshalling rest of event
    
    * Update comments, tests
    
    * Remove VersionedEvent struct left over from before
    
    * Update comments
    
    * Prefix header name tags with underscore

---
+++ headeredevent.go
@@ -0,0 +1,89 @@
+package gomatrixserverlib
+
+import (
+	"encoding/json"
+	"reflect"
+
+	"github.com/tidwall/sjson"
+)
+
+// HeaderedEventHeader contains header fields for an event that contains
+// additional metadata, e.g. room version. IMPORTANT NOTE: All fields in
+// this struct must have a "json:" name tag or otherwise the reflection
+// code for marshalling and unmarshalling headered events will not work.
+// They must be unique and not overlap with a name tag from the Event
+// struct or otherwise panics may occur, so header  name tags are instead
+// prefixed with an underscore.
+type EventHeader struct {
+	RoomVersion RoomVersion `json:"_room_version"`
+}
+
+// HeaderedEvent is a wrapper around an Event that contains information
+// about the room version. All header fields will be added into the event
+// when marshalling into JSON and will be separated out when unmarshalling.
+type HeaderedEvent struct {
+	EventHeader
+	Event
+}
+
+// UnmarshalJSON implements json.Unmarshaller
+func (e *HeaderedEvent) UnmarshalJSON(data []byte) error {
+	var err error
+	// First extract the headers from the JSON.
+	var m EventHeader
+	if err = json.Unmarshal(data, &m); err != nil {
+		return err
+	}
+	// Now strip any of the header fields from the JSON input data.
+	fields := reflect.TypeOf(e.EventHeader)
+	for i := 0; i < fields.NumField(); i++ {
+		if data, err = sjson.DeleteBytes(
+			data, fields.Field(i).Tag.Get("json"),
+		); err != nil {
+			return err
+		}
+	}
+	// Check what the room version is and prepare the Event struct for
+	// that specific version type.
+	switch m.RoomVersion {
+	case RoomVersionV1, RoomVersionV2:
+	case RoomVersionV3, RoomVersionV4, RoomVersionV5:
+	default:
+		return UnsupportedRoomVersionError{m.RoomVersion}
+	}
+	// Finally, unmarshal the remaining event JSON (less the headers)
+	// into the event struct.
+	if err := json.Unmarshal(data, &e.Event); err != nil {
+		return err
+	}
+	// At this point unmarshalling is complete.
+	return nil
+}
+
+// MarshalJSON implements json.Marshaller
+func (e HeaderedEvent) MarshalJSON() ([]byte, error) {
+	// First marshal the event struct itself.
+	content, err := json.Marshal(e.Event)
+	if err != nil {
+		return []byte{}, err
+	}
+	// Now jump through the fields of the header struct and add them
+	// in separately. This is needed because of the way that Go handles
+	// function overloading on embedded types, since Event also
+	// implements custom marshalling and unmarshalling functions.
+	// Doing this also ensures the least number of changes to Event
+	// references elsewhere.
+	fields := reflect.TypeOf(e.EventHeader)
+	values := reflect.ValueOf(e.EventHeader)
+	for i := 0; i < fields.NumField(); i++ {
+		if content, err = sjson.SetBytes(
+			content,
+			fields.Field(i).Tag.Get("json"),
+			values.Field(i).Interface(),
+		); err != nil {
+			return []byte{}, err
+		}
+	}
+	// Return the newly marshalled JSON.
+	return content, nil
+}
