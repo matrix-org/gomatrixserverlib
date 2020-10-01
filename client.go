2bfdd2727caed510cd4d4e1f3545a9ce2ac64353
Neil Alexander <neilalexander@users.noreply.github.com>
2020-10-01 11:02:50 +0100

    Add SetUserAgent to Client

--- client.go
+++ client.go
@@ -39,7 +39,8 @@ const requestTimeout time.Duration = time.Duration(30) * time.Second
 // A Client makes request to the federation listeners of matrix
 // homeservers
 type Client struct {
-	client http.Client
+	client    http.Client
+	userAgent string
 }
 
 // UserInfo represents information about a user.
@@ -143,6 +144,12 @@ func (f *federationTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 
 	// just return the most recent error
 	return nil, err
+}
+
+// SetUserAgent sets the user agent string that is sent in the headers of
+// outbound HTTP requests.
+func (fc *Client) SetUserAgent(ua string) {
+	fc.userAgent = ua
 }
 
 // LookupUserInfo gets information about a user from a given matrix homeserver
@@ -395,6 +402,9 @@ func (fc *Client) DoHTTPRequest(ctx context.Context, req *http.Request) (*http.R
 	})
 	logger.Trace("Outgoing request")
 	newCtx := util.ContextWithLogger(ctx, logger)
+	if fc.userAgent != "" {
+		req.Header.Set("User-Agent", fc.userAgent)
+	}
 
 	start := time.Now()
 	resp, err := fc.client.Do(req.WithContext(newCtx))
