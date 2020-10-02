bf5ee46d596c2229b5e6ae9d90137335960fb0bc
Neil Alexander <neilalexander@users.noreply.github.com>
2020-10-02 09:28:32 +0100

    Update error text

--- request.go
+++ request.go
@@ -256,7 +256,7 @@ func readHTTPRequest(req *http.Request) (*FederationRequest, error) { // nolint:
 	if len(content) != 0 {
 		mimetype, _, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
 		if err != nil {
-			return nil, fmt.Errorf("gomatrixserverlib: Failed to parse Content-Type header: %w", err)
+			return nil, fmt.Errorf("gomatrixserverlib: The request had an invalid Content-Type header: %w", err)
 		}
 		if mimetype != "application/json" {
 			return nil, fmt.Errorf("gomatrixserverlib: The request must be \"application/json\" not %q", mimetype)
