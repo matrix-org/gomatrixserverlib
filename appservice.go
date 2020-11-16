10b60cb10cf457a4ca1fc038f6014ca5c09a69c0
Neil Alexander <neilalexander@users.noreply.github.com>
2020-11-16 13:32:40 +0000

    Fix ApplicationServiceTransaction

--- appservice.go
+++ appservice.go
@@ -18,5 +18,5 @@ package gomatrixserverlib
 // ApplicationServiceTransaction is the transaction that is sent off to an
 // application service.
 type ApplicationServiceTransaction struct {
-	Events []Event `json:"events"`
+	Events []*Event `json:"events"`
 }
