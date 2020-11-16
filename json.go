7e6ff2f5a34ba69ed62d320f6d0b514d634e1f56
S7evinK <tfaelligen@gmail.com>
2020-11-16 11:13:18 +0100

    Make canonical json error public (#239)
    * Make canonical json error public
    
    Signed-off-by: Till Faelligen <tfaelligen@gmail.com>
    
    * Rename variable to make CI happy

--- json.go
+++ json.go
@@ -56,6 +56,8 @@ func EnforcedCanonicalJSON(input []byte, roomVersion RoomVersion) ([]byte, error
 	return CanonicalJSON(input)
 }
 
+var ErrCanonicalJSON = errors.New("value is outside of safe range")
+
 func verifyEnforcedCanonicalJSON(input []byte) error {
 	valid := true
 	res := gjson.ParseBytes(input)
@@ -77,7 +79,7 @@ func verifyEnforcedCanonicalJSON(input []byte) error {
 	}
 	res.ForEach(iter)
 	if !valid {
-		return errors.New("value is outside of safe range")
+		return ErrCanonicalJSON
 	}
 	return nil
 }
