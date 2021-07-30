6b8a1f30e46e8751fdfdd783d6c3cbabab8b1733
Neil Alexander <neilalexander@users.noreply.github.com>
2021-07-30 14:30:43 +0100

    Update DeviceListUpdateEvent

--- device_update.go
+++ device_update.go
@@ -2,11 +2,11 @@ package gomatrixserverlib
 
 // DeviceListUpdateEvent is https://matrix.org/docs/spec/server_server/latest#m-device-list-update-schema
 type DeviceListUpdateEvent struct {
-	UserID            string       `json:"user_id"`
-	DeviceID          string       `json:"device_id"`
-	DeviceDisplayName string       `json:"device_display_name"`
-	StreamID          int          `json:"stream_id"`
-	PrevID            []int        `json:"prev_id"`
-	Deleted           bool         `json:"deleted"`
-	Keys              []DeviceKeys `json:"keys"`
+	UserID            string      `json:"user_id"`
+	DeviceID          string      `json:"device_id"`
+	DeviceDisplayName string      `json:"device_display_name,omitempty"`
+	StreamID          int         `json:"stream_id"`
+	PrevID            []int       `json:"prev_id,omitempty"`
+	Deleted           bool        `json:"deleted,omitempty"`
+	Keys              *DeviceKeys `json:"keys,omitempty"`
 }
