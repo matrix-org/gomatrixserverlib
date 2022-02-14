28386a1973b401cb8a59f872de6308809f7060e2
Till Faelligen <tfaelligen@gmail.com>
2022-02-14 14:28:59 +0100

    Revert changes & only add ProxyFromEnvironment

--- client.go
+++ client.go
@@ -170,14 +170,16 @@ func (f *federationTripper) getTransport(tlsServerName string) (transport http.R
 
 	// Create the transport if we don't have any for this TLS server name.
 	if transport, ok = f.transports[tlsServerName]; !ok {
-		tr := http.DefaultTransport.(*http.Transport).Clone()
-		tr.DisableKeepAlives = !f.keepAlives
-		tr.TLSClientConfig = &tls.Config{
-			ServerName:         tlsServerName,
-			InsecureSkipVerify: f.skipVerify,
+		tr := &http.Transport{
+			DisableKeepAlives: !f.keepAlives,
+			TLSClientConfig: &tls.Config{
+				ServerName:         tlsServerName,
+				InsecureSkipVerify: f.skipVerify,
+			},
+			Dial:        federationTripperDialer.Dial, // nolint: staticcheck
+			DialContext: federationTripperDialer.DialContext,
+			Proxy:       http.ProxyFromEnvironment,
 		}
-		tr.Dial = federationTripperDialer.Dial // nolint: staticcheck
-		tr.DialContext = federationTripperDialer.DialContext
 		if f.dnsCache != nil {
 			tr.DialContext = f.dnsCache.DialContext
 		}
