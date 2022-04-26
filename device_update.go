333da1781008f8182d5c3653b8180ad8c82679c3
Neil Alexander <neilalexander@users.noreply.github.com>
2022-04-26 11:10:31 +0100

    Update more field ordering

--- device_update.go
+++ device_update.go
@@ -4,11 +4,11 @@ import "encoding/json"
 
 // DeviceListUpdateEvent is https://matrix.org/docs/spec/server_server/latest#m-device-list-update-schema
 type DeviceListUpdateEvent struct {
-	UserID            string          `json:"user_id"`
+	Deleted           bool            `json:"deleted,omitempty"`
+	DeviceDisplayName string          `json:"device_display_name,omitempty"`
 	DeviceID          string          `json:"device_id"`
-	DeviceDisplayName string          `json:"device_display_name"`
+	Keys              json.RawMessage `json:"keys,omitempty"`
+	PrevID            []int64         `json:"prev_id,omitempty"`
 	StreamID          int64           `json:"stream_id"`
-	PrevID            []int64         `json:"prev_id"`
-	Deleted           bool            `json:"deleted"`
-	Keys              json.RawMessage `json:"keys"`
+	UserID            string          `json:"user_id"`
 }
