16c0e1480bb370ecf04f242b05ce7177c0b9b614
Devon Hudson <devonhudson@librem.one>
2023-01-18 09:48:17 -0700

    Parameterize userid tests

--- user_test.go
+++ user_test.go
@@ -17,157 +17,73 @@ func TestEmptyFails(t *testing.T) {
 	}
 }
 
-func TestBasicValidSucceeds(t *testing.T) {
-	localpart := defaultLocalpart
-	domain := defaultDomain
-	raw := fmt.Sprintf("@%s:%s", localpart, domain)
+func TestValidUserIDs(t *testing.T) {
+	tests := map[string]struct {
+		localpart        string
+		domain           string
+		allowHistoricIDs bool
+	}{
+		"basic":                    {localpart: defaultLocalpart, domain: defaultDomain, allowHistoricIDs: false},
+		"extensive_local":          {localpart: "abcdefghijklmnopqrstuvwxyz0123456789._=-/", domain: defaultDomain, allowHistoricIDs: false},
+		"extensive_local_historic": {localpart: "!\"#$%&'()*+,-./0123456789;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~", domain: defaultDomain, allowHistoricIDs: true},
+		"domain_with_port":         {localpart: defaultLocalpart, domain: "domain.org:80", allowHistoricIDs: false},
+	}
 
-	userID, err := gomatrixserverlib.NewUserID(raw, false)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			raw := fmt.Sprintf("@%s:%s", tc.localpart, tc.domain)
 
-	if err != nil {
-		t.Fatalf("valid userID should not fail")
-	}
-	if userID.Local() != localpart {
-		t.Fatalf("Localpart - Expected: %s Actual: %s ", localpart, userID.Local())
-	}
-	if userID.Domain() != gomatrixserverlib.ServerName(domain) {
-		t.Fatalf("Domain - Expected: %s Actual: %s ", gomatrixserverlib.ServerName(domain), userID.Domain())
-	}
-	if userID.Raw() != raw {
-		t.Fatalf("Raw - Expected: %s Actual: %s ", raw, userID.Raw())
-	}
-}
+			userID, err := gomatrixserverlib.NewUserID(raw, tc.allowHistoricIDs)
 
-func TestExtensiveLocalpartSucceeds(t *testing.T) {
-	localpart := "abcdefghijklmnopqrstuvwxyz0123456789._=-/"
-	domain := defaultDomain
-	raw := fmt.Sprintf("@%s:%s", localpart, domain)
-
-	userID, err := gomatrixserverlib.NewUserID(raw, false)
-
-	if err != nil {
-		t.Fatalf("valid userID should not fail")
-	}
-	if userID.Local() != localpart {
-		t.Fatalf("Localpart - Expected: %s Actual: %s ", localpart, userID.Local())
-	}
-	if userID.Domain() != gomatrixserverlib.ServerName(domain) {
-		t.Fatalf("Domain - Expected: %s Actual: %s ", domain, userID.Domain())
-	}
-	if userID.Raw() != raw {
-		t.Fatalf("Raw - Expected: %s Actual: %s ", raw, userID.Raw())
+			if err != nil {
+				t.Fatalf("valid userID should not fail: %s", err.Error())
+			}
+			if userID.Local() != tc.localpart {
+				t.Fatalf("Localpart - Expected: %s Actual: %s ", tc.localpart, userID.Local())
+			}
+			if userID.Domain() != gomatrixserverlib.ServerName(tc.domain) {
+				t.Fatalf("Domain - Expected: %s Actual: %s ", gomatrixserverlib.ServerName(tc.domain), userID.Domain())
+			}
+			if userID.Raw() != raw {
+				t.Fatalf("Raw - Expected: %s Actual: %s ", raw, userID.Raw())
+			}
+		})
 	}
 }
 
-func TestExtensiveLocalpartHistoricalSucceeds(t *testing.T) {
-	localpart := "!\"#$%&'()*+,-./0123456789;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-	domain := defaultDomain
-	raw := fmt.Sprintf("@%s:%s", localpart, domain)
+func TestInvalidUserIDs(t *testing.T) {
+	tests := map[string]struct {
+		rawUserID string
+	}{
+		"too_long": {rawUserID: func() string {
+			userID := "@a:"
+			domain := ""
+			for i := 0; i < 255-len(userID)+1; i++ {
+				domain = domain + "a"
+			}
 
-	userID, err := gomatrixserverlib.NewUserID(raw, true)
+			raw := userID + domain
 
-	if err != nil {
-		t.Fatalf("valid userID should not fail")
-	}
-	if userID.Local() != localpart {
-		t.Fatalf("Localpart - Expected: %s Actual: %s ", localpart, userID.Local())
-	}
-	if userID.Domain() != gomatrixserverlib.ServerName(domain) {
-		t.Fatalf("Domain - Expected: %s Actual: %s ", domain, userID.Domain())
-	}
-	if userID.Raw() != raw {
-		t.Fatalf("Raw - Expected: %s Actual: %s ", raw, userID.Raw())
-	}
-}
-
-func TestDomainWithPortSucceeds(t *testing.T) {
-	localpart := defaultLocalpart
-	domain := "domain.org:80"
-	raw := fmt.Sprintf("@%s:%s", localpart, domain)
-
-	userID, err := gomatrixserverlib.NewUserID(raw, false)
-
-	if err != nil {
-		t.Fatalf("valid userID should not fail")
-	}
-	if userID.Local() != localpart {
-		t.Fatalf("Localpart - Expected: %s Actual: %s ", localpart, userID.Local())
-	}
-	if userID.Domain() != gomatrixserverlib.ServerName(domain) {
-		t.Fatalf("Domain - Expected: %s Actual: %s ", domain, userID.Domain())
-	}
-	if userID.Raw() != raw {
-		t.Fatalf("Raw - Expected: %s Actual: %s ", raw, userID.Raw())
-	}
-}
-
-func TestTooLongFails(t *testing.T) {
-	userID := "@a:"
-	domain := ""
-	for i := 0; i < 255-len(userID)+1; i++ {
-		domain = domain + "a"
+			if len(raw) <= 255 {
+				t.Fatalf("ensure the userid is greater than 255 (is %d) characters for this test", len(raw))
+			}
+			return raw
+		}()},
+		"no_leading_@":         {rawUserID: "localpart:domain"},
+		"no_colon":             {rawUserID: "@localpartdomain"},
+		"invalid_local_chars":  {rawUserID: "@local&part:domain"},
+		"invalid_domain_chars": {rawUserID: "@localpart:domain/"},
+		"no_local":             {rawUserID: "@:domain"},
 	}
 
-	raw := userID + domain
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			_, err := gomatrixserverlib.NewUserID(tc.rawUserID, false)
 
-	if len(raw) <= 255 {
-		t.Fatalf("ensure the userid is greater than 255 (is %d) characters for this test", len(raw))
-	}
-
-	_, err := gomatrixserverlib.NewUserID(raw, false)
-
-	if err == nil {
-		t.Fatalf("userID is not valid, it shouldn't parse")
-	}
-}
-
-func TestNoLeadingAtFails(t *testing.T) {
-	userID := "localpart:domain"
-
-	_, err := gomatrixserverlib.NewUserID(userID, false)
-
-	if err == nil {
-		t.Fatalf("userID is not valid, it shouldn't parse")
-	}
-}
-
-func TestNoColonFails(t *testing.T) {
-	userID := "@localpartdomain"
-
-	_, err := gomatrixserverlib.NewUserID(userID, false)
-
-	if err == nil {
-		t.Fatalf("userID is not valid, it shouldn't parse")
-	}
-}
-
-func TestInvalidLocalCharactersFails(t *testing.T) {
-	userID := "@local&part:domain"
-
-	_, err := gomatrixserverlib.NewUserID(userID, false)
-
-	if err == nil {
-		t.Fatalf("userID is not valid, it shouldn't parse")
-	}
-}
-
-func TestInvalidDomainFails(t *testing.T) {
-	userID := "@localpart:domain.com/"
-
-	_, err := gomatrixserverlib.NewUserID(userID, false)
-
-	if err == nil {
-		t.Fatalf("domain is not valid, it shouldn't parse")
-	}
-}
-
-func TestEmptyLocalpartFails(t *testing.T) {
-	userID := "@:domain"
-
-	_, err := gomatrixserverlib.NewUserID(userID, false)
-
-	if err == nil {
-		t.Fatalf("userID is not valid, it shouldn't parse")
+			if err == nil {
+				t.Fatalf("userID is not valid, it shouldn't parse")
+			}
+		})
 	}
 }
 
