02027a7f6b70918a4a6b8e7e349b20af4c2f43cc
Kegan Dougal <kegan@matrix.org>
2023-04-27 13:54:03 +0100

    Add helpers and fix linting

--- pdu.go
+++ pdu.go
@@ -15,6 +15,7 @@ type PDU interface {
 	RoomID() string
 	Redacts() string
 	PrevEventIDs() []string
+	PrevEvents() []EventReference // TODO: remove, used in Dendrite in (d *EventDatabase) StoreEvent
 	OriginServerTS() spec.Timestamp
 	Sender() string
 	EventReference() EventReference  // TODO: remove
@@ -22,4 +23,24 @@ type PDU interface {
 	JSON() []byte                    // TODO: remove
 	AuthEventIDs() []string          // TODO: remove
 	ToHeaderedJSON() ([]byte, error) // TODO: remove
+}
+
+// Convert a slice of concrete PDU implementations to a slice of PDUs. This is useful when
+// interfacing with GMSL functions which require []PDU.
+func ToPDUs[T PDU](events []T) []PDU {
+	result := make([]PDU, len(events))
+	for i := range events {
+		result[i] = events[i]
+	}
+	return result
+}
+
+// Temporary function to convert []PDU to []*Event. Panics if other types are given.
+// Remove this function when Dendrite no longer needs this (i.e it uses []PDU natively throughout)
+func TempCastToEvents(pdus []PDU) []*Event {
+	result := make([]*Event, len(pdus))
+	for i := range pdus {
+		result[i] = pdus[i].(*Event)
+	}
+	return result
 }
