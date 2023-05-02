782aebf832054e866f8c26f165dce7a674243a06
Kegan Dougal <kegan@matrix.org>
2023-05-02 11:12:47 +0100

    Add more helper functions on PDU

--- pdu.go
+++ pdu.go
@@ -11,19 +11,39 @@ type PDU interface {
 	StateKeyEquals(s string) bool
 	Type() string
 	Content() []byte
+	// JoinRule returns the value of the content.join_rule field if this event
+	// is an "m.room.join_rules" event.
+	// Returns an error if the event is not a m.room.join_rules event or if the content
+	// is not valid m.room.join_rules content.
+	JoinRule() (string, error)
+	// HistoryVisibility returns the value of the content.history_visibility field if this event
+	// is an "m.room.history_visibility" event.
+	// Returns an error if the event is not a m.room.history_visibility event or if the content
+	// is not valid m.room.history_visibility content.
+	HistoryVisibility() (HistoryVisibility, error)
 	Membership() (string, error)
 	PowerLevels() (*PowerLevelContent, error)
 	Version() RoomVersion
 	RoomID() string
 	Redacts() string
+	// Redacted returns whether the event is redacted.
+	Redacted() bool
 	PrevEventIDs() []string
 	PrevEvents() []EventReference // TODO: remove, used in Dendrite in (d *EventDatabase) StoreEvent
 	OriginServerTS() spec.Timestamp
+	// Redact redacts the event.
+	Redact()
 	Sender() string
 	Unsigned() []byte
 	// SetUnsigned sets the unsigned key of the event.
 	// Returns a copy of the event with the "unsigned" key set.
 	SetUnsigned(unsigned interface{}) (PDU, error)
+	// SetUnsignedField takes a path and value to insert into the unsigned dict of
+	// the event.
+	// path is a dot separated path into the unsigned dict (see gjson package
+	// for details on format). In particular some characters like '.' and '*' must
+	// be escaped.
+	SetUnsignedField(path string, value interface{}) error
 	// Sign returns a copy of the event with an additional signature.
 	Sign(signingName string, keyID KeyID, privateKey ed25519.PrivateKey) PDU
 	EventReference() EventReference  // TODO: remove
