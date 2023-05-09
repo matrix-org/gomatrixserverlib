1426ff5a325b6838b3df99f54df83ab2231199bf
Devon Hudson <devonhudson@librem.one>
2023-05-09 16:22:03 -0600

    Flush out matrix error tests

--- spec/matrixerror_test.go
+++ spec/matrixerror_test.go
@@ -19,26 +19,109 @@ import (
 	"testing"
 )
 
-func TestLimitExceeded(t *testing.T) {
-	e := LimitExceeded("too fast", 5000)
-	jsonBytes, err := json.Marshal(&e)
-	if err != nil {
-		t.Fatalf("TestLimitExceeded: Failed to marshal LimitExceeded error. %s", err.Error())
+func TestSimpleMatrixErrors(t *testing.T) {
+	tests := map[string]struct {
+		errorString  string
+		customErrMsg string
+		errorFunc    func(string) *MatrixError
+	}{
+		"m_unknown":                  {errorString: "M_UNKNOWN", errorFunc: Unknown},
+		"m_forbidden":                {errorString: "M_FORBIDDEN", errorFunc: Forbidden},
+		"m_bad_json":                 {errorString: "M_BAD_JSON", errorFunc: BadJSON},
+		"m_bad_alias":                {errorString: "M_BAD_ALIAS", errorFunc: BadAlias},
+		"m_not_json":                 {errorString: "M_NOT_JSON", errorFunc: NotJSON},
+		"m_not_found":                {errorString: "M_NOT_FOUND", errorFunc: NotFound},
+		"m_missing_token":            {errorString: "M_MISSING_TOKEN", errorFunc: MissingToken},
+		"m_unknown_token":            {errorString: "M_UNKNOWN_TOKEN", errorFunc: UnknownToken},
+		"m_weak_password":            {errorString: "M_WEAK_PASSWORD", errorFunc: WeakPassword},
+		"m_invalid_username":         {errorString: "M_INVALID_USERNAME", errorFunc: InvalidUsername},
+		"m_user_in_use":              {errorString: "M_USER_IN_USE", errorFunc: UserInUse},
+		"m_room_in_use":              {errorString: "M_ROOM_IN_USE", errorFunc: RoomInUse},
+		"m_exclusive":                {errorString: "M_EXCLUSIVE", errorFunc: ASExclusive},
+		"m_guest_access_forbidden":   {errorString: "M_GUEST_ACCESS_FORBIDDEN", errorFunc: GuestAccessForbidden},
+		"m_invalid_signature":        {errorString: "M_INVALID_SIGNATURE", errorFunc: InvalidSignature},
+		"m_invalid_param":            {errorString: "M_INVALID_PARAM", errorFunc: InvalidParam},
+		"m_missing_param":            {errorString: "M_MISSING_PARAM", errorFunc: MissingParam},
+		"m_unable_to_authorise_join": {errorString: "M_UNABLE_TO_AUTHORISE_JOIN", errorFunc: UnableToAuthoriseJoin},
+		"m_unsupported_room_version": {errorString: "M_UNSUPPORTED_ROOM_VERSION", errorFunc: UnsupportedRoomVersion},
+		"m_server_not_trusted":       {errorString: "M_SERVER_NOT_TRUSTED", errorFunc: NotTrusted, customErrMsg: "Untrusted server 'error msg'"},
 	}
-	want := `{"errcode":"M_LIMIT_EXCEEDED","error":"too fast","retry_after_ms":5000}`
-	if string(jsonBytes) != want {
-		t.Errorf("TestLimitExceeded: want %s, got %s", want, string(jsonBytes))
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			errorMsg := "error msg"
+			e := tc.errorFunc(errorMsg)
+			jsonBytes, err := json.Marshal(&e)
+			if err != nil {
+				t.Fatalf("Failed to marshal error. %s", err.Error())
+			}
+			if tc.customErrMsg != "" {
+				errorMsg = tc.customErrMsg
+			}
+			want := `{"errcode":"` + tc.errorString + `","error":"` + errorMsg + `"}`
+			if string(jsonBytes) != want {
+				t.Errorf("want %s, got %s", want, string(jsonBytes))
+			}
+		})
 	}
 }
 
-func TestForbidden(t *testing.T) {
-	e := Forbidden("you shall not pass")
+func TestInternalServerError(t *testing.T) {
+	e := InternalServerError()
 	jsonBytes, err := json.Marshal(&e)
 	if err != nil {
-		t.Fatalf("TestForbidden: Failed to marshal Forbidden error. %s", err.Error())
+		t.Fatalf("Failed to marshal error. %s", err.Error())
 	}
-	want := `{"errcode":"M_FORBIDDEN","error":"you shall not pass"}`
+	want := `{"Code":500,"JSON":{"errcode":"M_UNKNOWN","error":"Internal Server Error"},"Headers":null}`
 	if string(jsonBytes) != want {
-		t.Errorf("TestForbidden: want %s, got %s", want, string(jsonBytes))
+		t.Errorf("want %s, got %s", want, string(jsonBytes))
+	}
+}
+
+func TestLimitExceeded(t *testing.T) {
+	e := LimitExceeded("error msg", 500)
+	jsonBytes, err := json.Marshal(&e)
+	if err != nil {
+		t.Fatalf("Failed to marshal error. %s", err.Error())
+	}
+	want := `{"errcode":"M_LIMIT_EXCEEDED","error":"error msg","retry_after_ms":500}`
+	if string(jsonBytes) != want {
+		t.Errorf("want %s, got %s", want, string(jsonBytes))
+	}
+}
+
+func TestLeaveServerNoticeError(t *testing.T) {
+	e := LeaveServerNoticeError()
+	jsonBytes, err := json.Marshal(&e)
+	if err != nil {
+		t.Fatalf("Failed to marshal error. %s", err.Error())
+	}
+	want := `{"errcode":"M_CANNOT_LEAVE_SERVER_NOTICE_ROOM","error":"You cannot reject this invite"}`
+	if string(jsonBytes) != want {
+		t.Errorf("want %s, got %s", want, string(jsonBytes))
+	}
+}
+
+func TestWrongRoomKeysVersion(t *testing.T) {
+	e := WrongBackupVersionError("error msg")
+	jsonBytes, err := json.Marshal(&e)
+	if err != nil {
+		t.Fatalf("Failed to marshal error. %s", err.Error())
+	}
+	want := `{"errcode":"M_WRONG_ROOM_KEYS_VERSION","error":"Wrong backup version.","current_version":"error msg"}`
+	if string(jsonBytes) != want {
+		t.Errorf("want %s, got %s", want, string(jsonBytes))
+	}
+}
+
+func TestIncompatibleRoomVersion(t *testing.T) {
+	e := IncompatibleRoomVersion("error msg")
+	jsonBytes, err := json.Marshal(&e)
+	if err != nil {
+		t.Fatalf("Failed to marshal error. %s", err.Error())
+	}
+	want := `{"room_version":"error msg","error":"Your homeserver does not support the features required to join this room","errcode":"M_INCOMPATIBLE_ROOM_VERSION"}`
+	if string(jsonBytes) != want {
+		t.Errorf("want %s, got %s", want, string(jsonBytes))
 	}
 }
