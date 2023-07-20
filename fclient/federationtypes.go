7f19bec027e2807d5c79eb8d2ebe1e2c4676c5bb
Sam Wedgwood <28223854+swedgwood@users.noreply.github.com>
2023-07-20 14:06:51 +0100

    Remove remaining mentions of MSC2946 (#403)
    Remove mentions of MSC2946, as it it part of the spec. (For
    https://github.com/matrix-org/dendrite/pull/3134 ).
    
    Signed-off-by: `Sam Wedgwood <sam@wedgwood.dev>`

--- fclient/federationtypes.go
+++ fclient/federationtypes.go
@@ -528,24 +528,24 @@ type MSC2836EventRelationshipsResponse struct {
 	AuthChain gomatrixserverlib.EventJSONs `json:"auth_chain"`
 }
 
-// MSC2946Room represents a public room with additional metadata on the space directory
-type MSC2946Room struct {
+// RoomHierarchyRoom represents a public room with additional metadata on the space directory
+type RoomHierarchyRoom struct {
 	PublicRoom
-	ChildrenState  []MSC2946StrippedEvent `json:"children_state"`
-	AllowedRoomIDs []string               `json:"allowed_room_ids,omitempty"`
-	RoomType       string                 `json:"room_type"`
+	ChildrenState  []RoomHierarchyStrippedEvent `json:"children_state"`
+	AllowedRoomIDs []string                     `json:"allowed_room_ids,omitempty"`
+	RoomType       string                       `json:"room_type"`
 }
 
-// MSC2946SpacesResponse is the HTTP response body for the federation /unstable/spaces/{roomID} endpoint
+// RoomHierarchyResponse is the HTTP response body for the federation /unstable/spaces/{roomID} endpoint
 // See https://github.com/matrix-org/matrix-doc/pull/2946
-type MSC2946SpacesResponse struct {
-	Room                 MSC2946Room   `json:"room"`
-	Children             []MSC2946Room `json:"children"`
-	InaccessibleChildren []string      `json:"inaccessible_children"`
+type RoomHierarchyResponse struct {
+	Room                 RoomHierarchyRoom   `json:"room"`
+	Children             []RoomHierarchyRoom `json:"children"`
+	InaccessibleChildren []string            `json:"inaccessible_children"`
 }
 
-// MSC2946StrippedEvent is the format of events returned in the HTTP response body
-type MSC2946StrippedEvent struct {
+// RoomHierarchyStrippedEvent is the format of events returned in the HTTP response body
+type RoomHierarchyStrippedEvent struct {
 	Type           string          `json:"type"`
 	StateKey       string          `json:"state_key"`
 	Content        json.RawMessage `json:"content"`
